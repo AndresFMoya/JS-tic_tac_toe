47ad9daf4802572996973da8f184ae660d5c1a1e
Tiago Ferreira <tiferreira12@gmail.com>
2019-12-13 17:49:32 -0300

    feat: play method

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,13 @@
+const playerFactory = (name, symbol) => {
+  const getName = () => name;
+  const getSymbol = () => symbol;
+  return { getName, getSymbol };
+}
+
 const gameFactory = () => {
   const boardArray = [['', '', ''], ['', '', ''], ['', '', '']];
-  let player1 = undefined;
-  let player2 = undefined;
+  const player = [];
+  let turn = 0;
 
   const renderBoard = () => {
     boardArray.forEach((row, rowIndex) => {
@@ -14,20 +20,40 @@ const gameFactory = () => {
     });
   }
 
-  const startGame = () => {
-    player1 = playerFactory(document.getElementById('player1').value, 'X');
-    player2 = playerFactory(document.getElementById('player2').value, 'O');
+  const isGameFinished = () => {
+    ['00-01-02', '10-11-12', '20-21-22',
+      '00-10-20', '01-11-21', '02-12-22',
+      '00-11-22', '20-11-02'].forEach((winningPosition) => {
+      
+    });
   }
 
-  return { renderBoard, startGame };
+  const play = (move) => {
+    console.log('play')
+    boardArray[move[0]][move[1]] = player[turn].getSymbol();
+    turn = turn === 0 ? 1 : 0;
+    renderBoard();
+  }
+
+  const startGame = () => {
+    player[0] = playerFactory(document.getElementById('player1').value, 'X');
+    player[1] = playerFactory(document.getElementById('player2').value, 'O');
+    renderBoard();
+  }
+
+  return { startGame, renderBoard, play };
 };
 
-const playerFactory = (name, symbol) => {
-  const getName = () => name;
-  const getSymbol = () => symbol;
-  return { getName, getSymbol };
-}
 
 const game = gameFactory();
 game.renderBoard();
 // console.log(game.boardArray)
+
+// TODO: At first show only player names input and button to start game
+// TODO: Show the board and the turn player "Player X turn"
+// TODO: make the cells on the board clickable
+// TODO: have a game.play(player, cell) method
+// TODO: method to check if game is over (row, cols, diagonal or draw)
+// TODO: game.finish method "Congratulations Player X you win" + play again button > Player names
+// TODO: game.reset (private)
+// TODO: style the board + row + col + diagonal visual
